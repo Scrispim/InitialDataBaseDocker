cd6cfb3f372ba5c2cc290f5d28fb481a0e22f8e3
Sander Crispim <sandercrispim@hotmail.com>
2024-06-11 21:08:11 -0300

    new branch

--- setup.sql
+++ setup.sql
@@ -11,7 +11,13 @@ CREATE TABLE TestTable (
 );
 GO
 
-INSERT INTO TestTable (ID, Name) VALUES (1, 'Test Name');
+INSERT INTO TestTable (ID, Name) VALUES (1, 'Test Name 1');
+GO 
+
+INSERT INTO TestTable (ID, Name) VALUES (2, 'Test Name 2');
+GO 
+
+INSERT INTO TestTable (ID, Name) VALUES (3, 'Test Name 3');
 GO 
 
 PRINT 'AQUI VOCÊ COMUNICA O FINAL DA EXECUCAO';
